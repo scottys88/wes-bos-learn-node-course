007c9d860657aa53f4f205507b369223e2ae2f8e
scottys88 <scott_schubert@hotmail.com.au>
2018-05-09 22:35:05 +0930

    Up to 'Querying our database'

--- Learn-Node-master/Learn-Node-master/dang-thats-delicious/controllers/storeController.js
+++ Learn-Node-master/Learn-Node-master/dang-thats-delicious/controllers/storeController.js
@@ -3,6 +3,10 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
 	console.log(req.name);
+	req.flash('error', 'Something happened');
+	req.flash('info', 'Something happened');
+	req.flash('warning', 'Something happened');
+	req.flash('success', 'Something happened');
 	res.render('index');
 };
 
@@ -11,7 +15,8 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
+	const store = await (new Store(req.body)).save();
 	await store.save();
-	res.redirect('/');
+	req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+	res.redirect(`/store/${store.slug}`);
 };
